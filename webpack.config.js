c720eaa9e967fba2ad707fbb8d9d581d2569ea5b
Sami Keijonen <sami.keijonen@foxnet.fi>
2020-03-23 11:39:31 +0200

    Set url to false, handle fonts using Eleventy.

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,56 @@
-const path = require("path");
-const ManifestPlugin = require("webpack-manifest-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+/* global __dirname */
 
-const isDev = process.env.APP_ENV === "development";
+const path = require( 'path' );
+const ManifestPlugin = require( 'webpack-manifest-plugin' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 
-const baseFilename = isDev ? "index" : "index.[contenthash]";
+const isDev = process.env.APP_ENV === 'development';
+
+const baseFilename = isDev ? 'index' : 'index.[contenthash]';
 
 module.exports = {
-  entry: [
-    path.resolve(__dirname, "src", "js", "index.js"),
-    path.resolve(__dirname, "src", "css", "index.css"),
-  ],
-  output: {
-    path: path.resolve(__dirname, "dist", "assets"),
-    filename: `${baseFilename}.js`,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
-      },
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({ filename: `${baseFilename}.css` }),
-    new ManifestPlugin({ publicPath: "/assets/" }),
-  ],
+	entry: [
+		path.resolve( __dirname, 'src', 'js', 'index.js' ),
+		path.resolve( __dirname, 'src', 'css', 'index.css' ),
+	],
+	output: {
+		path: path.resolve( __dirname, 'dist', 'assets' ),
+		filename: `${ baseFilename }.js`,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [ '@babel/preset-env' ],
+					},
+				},
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							// We copy fonts etc. using Eleventy.
+							url: false,
+						},
+					},
+					{
+						loader: 'postcss-loader',
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin( { filename: `${ baseFilename }.css` } ),
+		new ManifestPlugin( { publicPath: '/assets/' } ),
+	],
 };
