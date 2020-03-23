7170c6c2d7f8f82462720730f71e607248ed9d4d
Sami Keijonen <sami.keijonen@foxnet.fi>
2020-03-23 11:40:12 +0200

    Add ecmaVersion just in case.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,13 @@
 module.exports = {
 	"extends": [ "plugin:@wordpress/eslint-plugin/esnext" ],
+	"parserOptions": {
+		"ecmaVersion": 2017
+	},
 	"globals": {
 		"document": true,
+		"module": true,
+		"process": true,
 		"require": true,
-		"window": true
+		"window": true,
 	}
 }
