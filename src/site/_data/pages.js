58bf768f5ba9c866dc7b3936616c7b4134854ce7
Sami Keijonen <sami.keijonen@foxnet.fi>
2020-03-23 11:40:34 +0200

    Clean up.

--- src/site/_data/pages.js
+++ src/site/_data/pages.js
@@ -1,11 +1,11 @@
-const axios = require('axios');
+const axios = require( 'axios' );
 
 module.exports = async function() {
 	return axios.get( 'https://11ty.foxnet.fi/wp-json/wp/v2/pages?per_page=100' )
-		.then(function (response) {
+		.then( function( response ) {
 			return response.data;
-		})
-		.catch(function(error) {
-			console.log(error);
-		});
-}
+		} )
+		.catch( function( error ) {
+			console.log( error ); /* eslint-disable-line */
+		} );
+};
